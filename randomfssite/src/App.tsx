17cc07b932f031f2ffc23bd5ff5f8a18f460c763
jyc-coder <jyc4648@gmail.com>
2022-10-11 08:08:27 +0900

    #2 - 기능 구현
    * 랜덤 명언 API를 사용하여 무작위로 명언을 출력하는 기능을 구현
    
    * axios의 get메서드`(/random)`를 호출해서 무작위로 명언이 출력됨
    
    * `page`변수의 값에 따라 api가 가져오는 데이터를 다르게 설정함
    ( main일때는 `/random` edit일 때는 `/`)
    
    * `page`변수가 `edit`일 때 모든 명언의 목록이 출력되게끔 `dataList`(api get메서드로 모든 명언의 데이터가 들어있음) 의 길이만큼 렌더링하여 출력함 이때 명언의 내용뿐만 아니라 수정/삭제 버튼이 각각 생성되어있음
    
    * 추가 버튼을 클릭하면 명언 목록 맨 아래에 입력창이 나타나고 이름과 내용을 작성하여 완료 버튼을 누르면 post메서드가 호출되면서 데이터가 추가됨 ( 이때 오류를 최대한 방지하기 위해 `setDataList`를 공백으로 만든 다음 get 메서드로 다시 렌더링되게 설계함
    
    *  수정 버튼을 클릭하면 `selectedData`의 존재 여부에 따라 렌더링이 변경되어 `input`창으로 변경되면서 사용자가 수정을 할수 있게됨. 내용 수정후 완료 버튼을 클릭하면 put 메서드가 호출되어 데이터가 수정됨
    
    * 삭제 버튼을 클릭하면 delete 메서드가 호출되면서 데이터가 제거됨

--- randomfssite/src/App.tsx
+++ randomfssite/src/App.tsx
@@ -1,10 +1,36 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Box from './components/Box';
 import Flex from './components/Flex';
-import {VscAdd, VscArrowLeft, VscEdit, VscListFlat, VscTrash} from 'react-icons/vsc';
+import {VscAdd, VscArrowLeft, VscCheck, VscClose, VscEdit, VscListFlat, VscTrash} from 'react-icons/vsc';
+import axios from 'axios';
+import Data from './interfaces/Data';
+
+axios.defaults.baseURL = 'http://localhost:8080/';
+
 function App() {
   const [page, setPage] = useState<'main' | 'edit'>('main');
+  const [nowData, setNowData] = useState<null | Data>(null);
+  const [dataList, setDataList] = useState<null | Data[]>(null);
+  const [createMode, setCreateMode] = useState(false);
+  const [createInp, setCreateInp] = useState<[string, string]>(["", ""]);
+  const [editInp, setEditInp] = useState<[string, string]>(['', '']);
+  const [selectedData, setSelectedData] = useState<string|null>(null)
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (page === 'main') {
+      axios
+        .get('/random')
+        .then((e) => setNowData(e.data))
+        .catch(() => setError('명언을 불러오지 못했습니다'));
+    } else {
+      axios
+        .get('/')
+        .then((e) => setDataList(e.data))
+        .catch(() => setError('명언을 불러오지 못했습니다'));
+    }
+  }, [page]);
 
   if (page === 'main')
     return (
@@ -32,20 +58,30 @@ function App() {
         >
           <Box fontSize={'24px'}>오늘의 명언</Box>
           <Flex
+            overflowX={'scroll'}
             mt="45px"
             mb="45px"
             border={'solid 1px #707070'}
-            width={'100%'}
             alignItems={'center'}
             justifyContent={'center'}
-            maxWidth={'1000px'}
+            width={'100%'}
             height={'160px'}
             textAlign={'center'}
             fontSize={'48px'}
           >
-            무슨 무슨 멋진 말 ~~~~~ 굿굿
+            {error.length > 0 && error}
+            <Box
+              width={'100%'}
+              px={'16px'}
+              style={{
+                whiteSpace: 'pre',
+              }}
+            >
+              {nowData?.message}
+              {'  '}
+            </Box>
           </Flex>
-          <Box fontSize={'24px'}>이름</Box>
+          <Box fontSize={'24px'}>{nowData?.author}</Box>
         </Flex>
       </>
     );
@@ -78,38 +114,152 @@ function App() {
           borderRadius={'10px'}
           alignItems={'center'}
           justifyContent={'center'}
-          onClick={() => setPage('main')}
+          onClick={() => setCreateMode((prev) => !prev)}
         >
-          <VscAdd color="white" fontSize={'24px'} />
+          {createMode ? <VscClose color="white" fontSize={'24px'} /> : <VscAdd color="white" fontSize={'24px'} />}
         </Flex>
       </Flex>
+      {dataList?.map((data, idx) => (
+        <Flex width="541px" height={'51px'} mb="23px" key={data.message}>
+          <Flex
+            border={'solid 1px #707070'}
+            flex={1}
+            overflowX="scroll"
+            style={{
+              whiteSpace: 'pre',
+            }}
+          >
+            {data.message === selectedData ? (
+              <>
+                <input
+                  value={editInp[0]}
+                  onChange={(event) => setEditInp((prev) => [event.target.value, prev[1]])}
+                />
+                <input
+                  value={editInp[1]}
+                  onChange={(event) => setEditInp((prev) => [prev[0], event.target.value])}
+                />
+              </>
+            ) : (
+              `[${data.author}] ${data.message}`
+            )}
+          </Flex>
+          <Flex
+            bg="#2D9CBF"
+            width={'74px'}
+            height={'51px'}
+            borderRadius={'10px'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            onClick={() => {
+              if (data.message === selectedData) {
+                  axios
+                    .put('/' + idx, {
+                      author: editInp[0],
+                      message: editInp[1],
+                    })
+                    .then(({data}) => {
+                      if (data.re) {
+                        setDataList([]);
+                        setEditInp(['', '']);
+                        setSelectedData(null);
+                        alert('수정 완료되었습니다!');
+                        axios
+                          .get('/')
+                          .then((e) => setDataList(e.data))
+                          .catch(() => setError('명언을 불러오지 못했습니다'));
+                      } else alert('수정이 실패하였습니다');
+                    });
+              }
+              setSelectedData(data.message);
+              setEditInp([data.author, data.message])
+            }}
+          >
+            {data.message === selectedData ? (
+              <VscCheck color="white" fontSize={'24px'} />
+            ) : (
+              <VscEdit color="white" fontSize={'24px'} />
+            )}
+          </Flex>
 
-      <Flex width="541px" height={'51px'} >
-        <Flex border={'solid 1px #707070'} flex={1}></Flex>
-        <Flex
-          bg="#2D9CBF"
-          width={'74px'}
-          height={'51px'}
-          borderRadius={'10px'}
-          alignItems={'center'}
-          justifyContent={'center'}
-          onClick={() => setPage('main')}
-        >
-          <VscEdit color="white" fontSize={'24px'} />
+          <Flex
+            bg="#BF2D2D"
+            width={'74px'}
+            height={'51px'}
+            borderRadius={'10px'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            onClick={() => {
+              if (window.confirm('정말 해당 명언을 제거 하시겠습니까?')) {
+                axios.delete('/' + idx).then(({data}) => {
+                  if (data.re) {
+                    setDataList([]);
+                    alert('제거 완료되었습니다!');
+                    axios
+                      .get('/')
+                      .then((e) => setDataList(e.data))
+                      .catch(() => setError('명언을 불러오지 못했습니다'));
+                  } else alert('제거가 실패하였습니다');
+                });
+              }
+            }}
+          >
+            <VscTrash color="white" fontSize={'24px'} />
+          </Flex>
         </Flex>
+      ))}
 
-        <Flex
-          bg="#BF2D2D"
-          width={'74px'}
-          height={'51px'}
-          borderRadius={'10px'}
-          alignItems={'center'}
-          justifyContent={'center'}
-          onClick={() => setPage('main')}
-        >
-          <VscTrash color="white" fontSize={'24px'} />
-        </Flex>
-      </Flex>
+      {createMode && (
+        <>
+          <Flex width="541px" height={'51px'} mb="23px">
+            <Flex
+              border={'solid 1px #707070'}
+              flex={1}
+              overflowX="scroll"
+              style={{
+                whiteSpace: 'pre',
+              }}
+            >
+              <input value={createInp[0]} onChange={(event) => setCreateInp((prev) => [event.target.value, prev[1]])} />
+              <input value={createInp[1]} onChange={(event) => setCreateInp((prev) => [prev[0], event.target.value])} />
+            </Flex>
+            <Flex
+              bg="#2D9CBF"
+              width={'74px'}
+              height={'51px'}
+              borderRadius={'10px'}
+              alignItems={'center'}
+              justifyContent={'center'}
+              onClick={() => {
+                if (createInp[0].length === 0 || createInp[1].length === 0) {
+                  alert('정상적인 값이 아닙니다.');
+                  return;
+                }
+
+                axios
+                  .post('/', {
+                    author: createInp[0],
+                    message: createInp[1],
+                  })
+                  .then(({data}) => {
+                    if (data.re) {
+                      setDataList([]);
+                      setCreateInp(['', '']);
+                      setCreateMode(false);
+                      alert('생성 완료되었습니다!');
+                      axios
+                        .get('/')
+                        .then((e) => setDataList(e.data))
+                        .catch(() => setError('명언을 불러오지 못했습니다'));
+                    } else alert('생성이 실패하였습니다');
+                  });
+              }}
+            >
+              <VscCheck color="white" fontSize={'24px'} />
+            </Flex>
+          </Flex>
+        </>
+      )}
     </Flex>
   );
 }
